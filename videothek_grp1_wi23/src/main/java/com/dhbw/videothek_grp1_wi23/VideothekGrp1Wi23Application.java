35b75ceb83070da47cdfd87d1c51dce07345ce4e
ARJON ISA <arjon.isa@sap.com>
2025-03-07 17:21:33 +0100

    added prototype

---
+++ videothek_grp1_wi23/src/main/java/com/dhbw/videothek_grp1_wi23/VideothekGrp1Wi23Application.java
@@ -0,0 +1,13 @@
+package com.dhbw.videothek_grp1_wi23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideothekGrp1Wi23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideothekGrp1Wi23Application.class, args);
+	}
+
+}
---
+++ videothek_grp1_wi23/src/test/java/com/dhbw/videothek_grp1_wi23/VideothekGrp1Wi23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dhbw.videothek_grp1_wi23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideothekGrp1Wi23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
